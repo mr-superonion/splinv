6480ab7955302600a9afc9904ed293a821b8f416
Xiangchong Li <xiangchong.li@ipmu.jp>
2021-12-09 13:52:38 +0900

    preparing to implement mcmc

--- python/detect3D.py
+++ python/detect3D.py
@@ -4,23 +4,17 @@ from scipy import ndimage as ndi
 def local_maxima_3D(data,npixt=2,npixp=1,threshold=1.):
     """Detects local maxima in a 3D array
 
-    Parameters
-    ---------
-    data : 3d ndarray
-    npixt : int
-        How many points on each dimension of transverse plane to use for the
-        comparison
+    Parameters:
+    data :  [3D ndarray]
+    npixt:  How many points on each dimension of transverse plane to use for the
+            comparison [int]
 
-    npixp : int
-        How many points on the line-of-sight dimension to use for the
-        comparison
+    npixp : How many points on the line-of-sight dimension to use for the
+            comparison [int]
 
-    Returns
-    -------
-    coords : ndarray
-        coordinates of the local maxima
-    values : ndarray
-        values of the local maxima
+    Returns:
+        coords: coordinates of the local maxima [ndarray]
+        values: values of the local maxima [ndarray]
     """
     sizet = 1 + 2 * npixt
     sizep = 1 + 2 * npixp
@@ -28,7 +22,7 @@ def local_maxima_3D(data,npixt=2,npixp=1,threshold=1.):
     footprint[npixp, npixt, npixt] = 0
     filtered    =   ndi.maximum_filter(data,footprint=footprint,mode='constant')
     mask_local_maxima = (data > filtered)& (data>threshold)
-    coords = np.asarray(np.where(mask_local_maxima)).T
+    coords = np.int_(np.asarray(np.where(mask_local_maxima)).T)
 
     footprint[npixp, npixt, npixt] = 1
     smoothed    =   ndi.convolve(data,weights=footprint,mode='constant')
@@ -38,23 +32,16 @@ def local_maxima_3D(data,npixt=2,npixp=1,threshold=1.):
 def local_minima_3D(data,npixt=2,npixp=2,threshold=1.):
     """Detects local minima in a 3D array
 
-    Parameters
-    ---------
-    data : 3d ndarray
-    npixt : int
-        How many points on each dimension of transverse plane to use for the
-        comparison
+    Parameters:
+    data :  [3D ndarray]
+    npixt:  How many points on each dimension of transverse plane to use for
+            the comparison [int]
+    npixp:  How many points on the line-of-sight dimension to use for the
+            comparison [int]
 
-    npixp : int
-        How many points on the line-of-sight dimension to use for the
-        comparison
-
-    Returns
-    -------
-    coords : ndarray
-        coordinates of the local minima
-    values : ndarray
-        values of the local minima
+    Returns:
+        coords : coordinates of the local minima [ndarray]
+        values : values of the local minima [ndarray]
     """
     sizet = 1 + 2 * npixt
     sizep = 1 + 2 * npixp
@@ -63,6 +50,6 @@ def local_minima_3D(data,npixt=2,npixp=2,threshold=1.):
 
     filtered = ndi.minimum_filter(data, footprint=footprint,mode='constant')
     mask_local_minima = (data < filtered)& (data<-threshold)
-    coords = np.asarray(np.where(mask_local_minima)).T
+    coords = np.int_(np.asarray(np.where(mask_local_minima)).T)
     values = data[mask_local_minima]
     return coords, values
