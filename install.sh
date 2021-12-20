07c336e18478593e84129a7d8a47c8af7bf7cbfc
Xiangchong Li <xiangchong.li@ipmu.jp>
2021-12-21 07:25:28 +0900

    better name

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ Dir="$homeSys/code/massMap_Private/"
 
 export PYTHONPATH="$Dir/ddmap/":$PYTHONPATH
 
-export PATH="$Dir/demo/sim/":$PATH
-export PYTHONPATH="$Dir/demo/sim/":$PYTHONPATH
+export PATH="$Dir/tasks/sim/":$PATH
+export PYTHONPATH="$Dir/tasks/sim/":$PYTHONPATH
 
-export PATH="$Dir/demo/s19a/":$PATH
-export PYTHONPATH="$Dir/demo/s19a/":$PYTHONPATH
+export PATH="$Dir/tasks/s19a/":$PATH
+export PYTHONPATH="$Dir/tasks/s19a/":$PYTHONPATH
 
 DirBase="$homeSys/code/FPFS_Private/FPFSBASE/"
 export PYTHONPATH="$DirBase/python/":$PYTHONPATH
