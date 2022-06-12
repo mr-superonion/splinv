df4c78bd29b8eab6853fb7bf8318c84f98e2827c
Xiangchong Li <xiangchong.li@ipmu.jp>
2022-06-12 23:37:03 +0900

    add function to plot 2D maps, and better documentations

--- splinv/grid.py
+++ splinv/grid.py
@@ -14,12 +14,16 @@ import json
 import numpy as np
 from .default import *
 from .maputil import GausAtom
+from astropy.wcs import WCS
 from astropy.cosmology import FlatLambdaCDM as Cosmo
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 class Cartesian():
     """
     pixlize in a TAN(Gnomonic)-prjected Cartesian Grid
-    Parameters:
+    Rotation and coordinate distortion are not implemented
+    Args:
         parser: parser
     """
     def __init__(self,parser):
@@ -33,6 +37,7 @@ class Cartesian():
         elif unit== 'arcsec':
             self.ratio= 1./60./60.
         self.scale= parser.getfloat('transPlane','scale')*self.ratio
+        self.unit=unit
 
         # Line-of-sight direction for background galaxies
         if parser.has_section('sources'):
@@ -62,12 +67,11 @@ class Cartesian():
             ny      =   parser.getint('transPlane','ny')
             xmin    =   parser.getfloat('transPlane','xmin')*self.ratio
             ymin    =   parser.getfloat('transPlane','ymin')*self.ratio
-            self.setupNaivePlane(nx,ny,xmin,ymin)
+            self.__init_tan_plane(nx,ny,xmin,ymin)
 
         ## Gaussian smoothing in the transverse plane
         if parser.has_option('transPlane','smooth_scale'):
-            self.sigma  =   parser.getfloat('transPlane','smooth_scale')\
-                            *self.ratio
+            self.sigma  =   parser.getfloat('transPlane','smooth_scale')*self.ratio
         else:
             self.sigma  =   -1
         self.sigma_pix  =   self.sigma/self.scale
@@ -98,14 +102,22 @@ class Cartesian():
         self.lensKernel =   None
         return
 
-    def setupNaivePlane(self,nx,ny,xmin,ymin):
+    def reset_smooth_scale(self,sigma):
+        """resets the smoothing scale
+        (input the sigma in the same unit as self.unit)
+        """
+        self.sigma  =   sigma*self.ratio
+        self.sigma_pix  =   self.sigma/self.scale
+        return
+
+    def __init_tan_plane(self,nx,ny,xmin,ymin):
         """setup Tan projection plane from basic parameters
         (no rotation,no flipping)
-        Parameters:
-            nx:     number of x bins [int]
-            ny:     number of y bins [int]
-            xmin:   minimum of x [deg]
-            ymin:   minimum of y [deg]
+        Args:
+            nx (int):       number of x bins [int]
+            ny (int):       number of y bins [int]
+            xmin (float):   minimum of x [deg]
+            ymin (float):   minimum of y [deg]
         """
         self.nx     =   nx
         self.ny     =   ny
@@ -128,11 +140,14 @@ class Cartesian():
     def setupTanPlane(self,ra=None,dec=None,pad=0.1,header=None):
         """setup Tan projection plane from (ra,dec) array or header
         (no rotation,no flipping)
-        Parameters:
-            ra:     array of ra to project  [deg]
-            dec:    array of dec to project [deg]
-            pad:    padding distance [degree]
-            header: header with projection information
+        Args:
+            ra (ndarray):   array of ra to project  [deg]
+            dec (ndarray):  array of dec to project [deg]
+            pad (float):    padding angular distance [deg]
+            header (dict):  header with projection information
+        Returns:
+            ra (ndarray):   ra in the projected plane
+            dec (ndarray):  dec in the projected plane
         """
         if (ra is not None) and (dec is not None):
             # first try
@@ -149,6 +164,8 @@ class Cartesian():
             self.cosdec0=   np.cos(self.dec0/180.*np.pi)
             self.sindec0=   np.sin(self.dec0/180.*np.pi)
             x,y         =   self.project_tan(ra,dec)
+            xt          =   (np.max(x)+np.min(x))/2.
+            yt          =   (np.max(y)+np.min(y))/2.
 
             dxmin   =   self.ra0-(np.min(x)-pad)
             dxmax   =   (np.max(x)+pad)-self.ra0
@@ -190,10 +207,13 @@ class Cartesian():
     def project_tan(self,ra,dec,pix=False):
         """TAN(Gnomonic)-prjection of sky coordiantes
         (no rotation,no flipping)
-        Parameters:
-            ra:     array of ra to project  [deg]
-            dec:    array of dec to project [deg]
-            pix:    return in unit of pixel or not [bool]
+        Args:
+            ra (ndarray):   sky coordinate---ra [deg]
+            dec (ndarray):  sky coordinate---dec [deg]
+            pix (bool):     return in unit of pixel or not
+        Returns:
+            x1 (ndarray):   pixel coordinate x1 [deg]
+            x2 (ndarray):   pixel coordinate x2 [deg]
         """
         rr      =   180.0/np.pi
         sindec  =   np.sin(dec/rr)
@@ -213,17 +233,18 @@ class Cartesian():
     def iproject_tan(self,x1,x2,pix=False):
         """inverse TAN(Gnomonic)-prjection of pixel coordiantes
         (no rotation,no flipping)
-        Parameters:
-            x1:     array of x1 pixel coord [deg]
-            x2:     array of x2 pixel coord [deg]
-            pix:    input in unit of pixel or not [bool]
+        Args:
+            x1 (ndarray):   x1 pixel coord [deg]
+            x2 (ndarray):   x2 pixel coord [deg]
+            pix (bool):     input in unit of pixel or not (default=False)
+        Returns:
+            ra (ndarray):   sky coordinate [deg]
+            dec (ndarray):  sky coordinate [deg]
         """
-
         if pix:
             # transform to in unit of degree
             x1  =   x1*self.scale+self.xbound[0]
             x2  =   x2*self.scale+self.ybound[0]
-
         # unit
         rr      =   180.0/np.pi
         dx      =   (x1-self.ra0)/rr
@@ -245,21 +266,22 @@ class Cartesian():
         dec     =   np.arcsin(self.sindec0*cosC+self.cosdec0*sinC*sinP)*rr
         return ra,dec
 
-    def pixelize_data(self,x,y,z,v=None,ws=None,method='FFT'):
+    def pixelize_data(self,x,y,z,v=None,ws=None,smooth_method='pix',return_mask=False):
         """pixelize catalog into the cartesian grid
-        Parameters:
-            x:  array
-                ra of sources. (deg)
-            y:  array
-                dec of sources. (deg)
-            z:  array
-                redshifts of sources.
-            v:  array
-                measurements. (e.g., shear, kappa)
-            ws: array [defalut: None]
-                weights.
-            method: string [default: 'FFT']
-                method to convolve with the Gaussian kernel
+        Args:
+            x (ndarray):    ra of sources [deg]
+            y (ndarray):    dec of sources [deg]
+            z (ndarray):    redshifts of sources
+            v (ndarray):    measurements. (e.g., shear, kappa, default: None)
+            ws (ndarray):   galaxy weights (default: None).
+            smooth_method (str):
+                            method to convolve with the Gaussian kernel
+                            ('gal' or 'pix' default: 'pix')
+            return_mask (bool):
+                            return mask or not (default: False)
+        Returns:
+            out (ndarray):  pixelized data
+                            when v==None, returns the galaxy numbers in pixels
         """
         if z is None:
             # This is for 2D pixeliztion
@@ -268,17 +290,21 @@ class Cartesian():
         if ws is None:
             # Without weight
             ws = np.ones(len(x))
-        if method=='sample':
+        if smooth_method=='gal':
+            # conduct galaxy level smooth: smooth then pixelize (slow)
             if self.sigma>0.:
-                return self._pixelize_data_sample(x,y,z,v,ws)
+                return self._pixelize_data_gal(x,y,z,v,ws)
             else:
-                raise ValueError("when method is 'sample', smooth_scale must >0")
-        elif method=='FFT':
-            return self._pixelize_data_FFT(x,y,z,v,ws)
+                raise ValueError("when method is 'gal', smooth_scale must >0")
+        elif smooth_method=='pix':
+            # conduct pixel level smooth: pixelize then smooth (fast)
+            return self._pixelize_data_pix(x,y,z,v,ws,return_mask)
         else:
-            raise ValueError("method must be 'FFT' or 'sample'")
+            raise ValueError("method must be 'pix' or 'gal'")
 
-    def _pixelize_data_FFT(self,x,y,z,v=None,ws=np.empty(0)):
+    def _pixelize_data_pix(self,x,y,z,v=None,ws=np.empty(0),return_mask=False):
+        """pixelize data and then do smoothing on galaxy level (fast)
+        """
         if v is not None:
             # pixelize value field
             dataOut =   np.histogramdd((z,y,x),bins=(self.zbound,self.ybound,self.xbound),weights=v*ws)[0]
@@ -286,7 +312,7 @@ class Cartesian():
             dataOut =   []
         # pixelize weight field
         weightOut=  np.histogramdd((z,y,x),bins=(self.zbound,self.ybound,self.xbound),weights=ws)[0]
-        if self.sigma_pix>0.01:
+        if self.sigma_pix>0.01: #0.01 arcmin
             # Gaussian Kernel in Fourier space
             # (normalize to flux-1)
             gausKer =   GausAtom(ny=self.ny,nx=self.nx,sigma=self.sigma_pix,fou=True)
@@ -298,16 +324,27 @@ class Cartesian():
             if v is not None:
                 dataOut=np.fft.ifft2(np.fft.fft2(dataOut)*gausKer).real
 
+        mask            =   weightOut>0.1
+        thres           =   np.mean(weightOut[mask])/10.
+        mask            =   weightOut>thres
+        weightOut[~mask]=   0
         if v is not None:
             # avoid weight is zero
-            mask            =   weightOut>0.1
             dataOut[mask]   =   dataOut[mask]/weightOut[mask]
             dataOut[~mask]  =   0.
-            return dataOut
+            if not return_mask:
+                return dataOut
+            else:
+                return dataOut,mask
         else:
-            return weightOut
+            if not return_mask:
+                return weightOut
+            else:
+                return weightOut,mask
 
-    def _pixelize_data_sample(self,x,y,z,v=None,ws=np.empty(0)):
+    def _pixelize_data_gal(self,x,y,z,v=None,ws=np.empty(0)):
+        """do smoothing on galaxy level and then pixelize data (slow)
+        """
         xbin=   np.int_((x-self.xbound[0])/self.scale)
         ybin=   np.int_((y-self.ybound[0])/self.scale)
         if v is not None:
@@ -343,20 +380,17 @@ class Cartesian():
 
     def lensing_kernel(self,poz_grids=None,poz_data=None,poz_best=None,z_dens=None,deltaIn=True):
         """ Lensing kernel
-
-        Parameters:
-            poz_grids:  array
-                poz's bin; if None, do not include any photoz uncertainty
-            poz_best:   array, in shape of len(galaxy)
-                galaxy's best photoz measurements, used for galaxy binning
-            poz_data:   2D array, in shape of (len(galaxy), len(poz_grids))
-                galaxy's POZ measurements, used for deriving lensing kernel
-            z_dens:    array
-                average POZ in source bins
-            deltaIn:    bool [default: True]
-                is mapping from delta to kappa (True) or from mass to kappa (False)
+        Args:
+            poz_grids (ndarray):poz's bin; if None, do not include any photoz uncertainty
+                                (default=None)
+            poz_best (ndarray): galaxy's best photoz measurements, used for galaxy binning
+            poz_data (ndarray): galaxy's POZ measurements, used for deriving lensing kernel
+                                shape=(len(galaxy), len(poz_grids))
+            z_dens (ndarray):   average POZ in source bins
+            deltaIn (bool):     mapping from delta to kappa (True) or from mass to kappa (False)
+                                (default=True)
         Returns:
-            lensing kernel
+            out (ndarray):      lensing kernel
         """
 
         assert (poz_data is not None)==(poz_best is not None), \
@@ -365,7 +399,7 @@ class Cartesian():
             'number of lens plane and source plane'
         if self.nzl<=1:
             return np.ones((self.nz,self.nzl))
-        lensKernel =   np.zeros((self.nz,self.nzl))
+        out =   np.zeros((self.nz,self.nzl))
         if poz_grids is None:
             # Without z_density or photoz posterier
             for i,zl in enumerate(self.zlcgrid):
@@ -380,10 +414,10 @@ class Cartesian():
                     # Surface masss density in lens bin
                     rhoM_ave=   self.cosmo.critical_density(zl).to_value(unit=rho_unt)*self.cosmo.Om(zl)
                     DaBin   =   self.cosmo.angular_diameter_distance_z1z2(self.zlbound[i],self.zlbound[i+1]).value
-                    lensKernel[:,i]=kl*rhoM_ave*DaBin
+                    out[:,i]=kl*rhoM_ave*DaBin
                 else:
                     # When the input is M_200/1e14
-                    lensKernel[:,i]=kl*1e14
+                    out[:,i]=kl*1e14
         else:
             # with z_dens or photo-z posterier
             lensK =   np.zeros((len(poz_grids),self.nzl))
@@ -403,6 +437,8 @@ class Cartesian():
                 else:
                     lensK[:,i]= kl*1e14
             if z_dens is None:
+                if poz_data is None:
+                    raise ValueError('Cannot estimate n(z) if poz_data is None')
                 # Prepare the n(z) if it is not an input.
                 # using the stacked posterier of photo-z
                 assert len(poz_data)==len(poz_best)
@@ -414,13 +450,15 @@ class Cartesian():
                     z_dens[iz,:]=   np.average(poz_data[tmp_msk],axis=0)
 
             self.z_dens =   z_dens
-            lensKernel  =   z_dens.dot(lensK)
-            self.lensKernel=lensKernel
-        return lensKernel
+            out  =   z_dens.dot(lensK)
+            self.lensKernel=out
+        return out
 
     def lensing_kernel_infty(self):
         """Mapping from an average delta in a lens redshfit bin
         to an average kappa in a source redshift at z=+infty
+        Returns:
+            lensKernel (ndarray):     lensing kernel
         """
         lensKernel =np.zeros((self.nz,self.nzl))
         kl      =   self.cosmo.angular_diameter_distance_z1z2(0.,self.zlcgrid).value*four_pi_G_over_c_squared()
@@ -429,3 +467,82 @@ class Cartesian():
         DaBin   =   self.cosmo.angular_diameter_distance_z1z2(self.zlbound[:-1],self.zlbound[1:]).value
         lensKernel= kl*rhoM_ave*DaBin
         return lensKernel
+
+    def make_plot2D(self,mapIn,mask=None,title=''):
+        """Plot the 2D pixelized map
+        Args:
+            mapIn (ndarray):    input 2D map
+            mask (ndarray):     input 2D mask
+            title (str):        title of the plot, e.g., 'HSC: g1', 'B-mode'
+        Returns:
+            fig1 (figure):      matplotlib figure for 2D map
+            fig2 (figure):      matplotlib figure for pixel histogram
+        """
+        dim=len(mapIn.shape)
+        if dim!=2:
+            raise ValueError('Input map should be 2D, but get a %dD array' %dim)
+        # prepare the WCS for 2D map
+        wcs_input_dict = {
+            'CTYPE1': 'RA---TAN',
+            'CUNIT1': 'deg',
+            'CDELT1': 1./60.,
+            'CRPIX1': self.nx//2+1,
+            'CRVAL1': self.xcgrid[self.nx//2],
+            'NAXIS1': self.nx,
+            'CTYPE2': 'DEC--TAN',
+            'CUNIT2': 'deg',
+            'CDELT2': 1./60.,
+            'CRPIX2': self.ny//2+1, #fits 1 based
+            'CRVAL2': self.ycgrid[self.ny//2],
+            'NAXIS2': self.ny
+        }
+        wcs_header = WCS(wcs_input_dict)
+        pratio=int(mapIn.shape[1]/mapIn.shape[0]+0.5)
+        # pixel value list
+        if mask is not None:
+            assert mask.shape==mapIn.shape, 'The input map and mask have different shape'
+            tt=np.ravel(np.ravel(mapIn))[np.ravel(mask)]
+        else:
+            tt=np.ravel(np.ravel(mapIn))
+        vmin,vmax=np.percentile(tt,q=[0.02,99.98])
+        # determine the color bar
+        if vmin*vmax>=0:
+            cmap='RdYlBu_r'
+            gcolor='white'
+            norm=mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        else:
+            cmap='seismic'
+            gcolor='black'
+            if np.abs(vmin+vmax)<max(-vmin,vmax)*0.3:
+                vmax2=(vmax-vmin)/2.
+                vmin2=(vmax-vmin)/-2.
+                vmax=vmax2*1.25;vmin=vmin2*1.25
+            norm=mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        # plot for 2D map
+        interpolate=None
+        fig1=plt.figure(figsize=(3*pratio,3))
+        ax=fig1.add_subplot(1,1,1,projection=wcs_header,aspect='equal')
+        ax.set_title(title,fontsize=20)
+        imap=ax.imshow(mapIn,origin='lower',norm=norm,cmap=cmap,interpolation=interpolate)
+        ax.grid(color=gcolor, ls='dotted')
+        ax.coords[0].set_major_formatter('d.d')
+        ax.set_xlabel('ra [deg]',fontsize=20)
+        ax.set_ylabel('dec [deg]',fontsize=20)
+        fig1.colorbar(imap)
+        fig1.tight_layout()
+
+        # plot for pixle histogram
+        fig2=plt.figure(figsize=(7,6))
+        ax2=fig2.add_subplot(1,1,1)
+        ax2.hist(tt,bins=20,histtype='step')
+        ax2.set_yscale('log')
+        if vmin*vmax<0.:
+            ax2.set_xlim(vmin*1.6,vmax*1.6)
+        else:
+            ax2.set_xlim(vmin,vmax)
+        ax2.set_ylabel('PDF')
+        ax2.set_xlabel(title.split(':')[-1])
+        ax2.set_title(title,fontsize=20)
+        ax2.grid()
+        fig2.tight_layout()
+        return fig1,fig2
